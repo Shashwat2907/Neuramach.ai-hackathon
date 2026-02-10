dac54b8cc7affef5f03fb62b96a15d4be832cba6
Shashwat2907 <work.shashwatchoudhary@gmail.com>
2026-02-11 00:00:10 +0530

    Initial commit-MVP Frontend v1

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault(); // Stop page from reloading
+
+    const email = document.getElementById('emailInput').value;
+    const btn = document.getElementById('loginBtn');
+    const btnText = document.getElementById('btnText');
+    const spinner = document.getElementById('loadingSpinner');
+
+    // 1. Show Loading State
+    btnText.style.display = 'none';
+    spinner.classList.remove('hidden');
+    btn.style.opacity = '0.7';
+    btn.disabled = true;
+
+    // 2. Simulate "Talking to Server" (Fake Delay)
+    setTimeout(() => {
+        // Success!
+        alert("Welcome to the Vault, " + email);
+        
+        // Reset button
+        btnText.style.display = 'block';
+        spinner.classList.add('hidden');
+        btn.style.opacity = '1';
+        btn.disabled = false;
+        
+        // HERE IS WHERE WE WOULD GO TO THE DASHBOARD
+         window.location.href = "dashboard.html";
+        
+    }, 1500); // 1.5 seconds delay
+});
+
